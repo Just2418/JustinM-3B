da5e6e3c67838139695ddd7dbc76719b0a17dd58
Justin Manarang <justinmanarang24@gmail.com>
2024-05-22 18:01:38 +0800

    Post-Reources has successfully Updated!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,31 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+                        <h5 class="card-title">{{ __('Dashboard') }}</h5>
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ __('Total Number of Posts') }}</h5>
+                                        <p class="card-text">{{ $totalPosts }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ __('Total Number of Unpublished Posts') }}</h5>
+                                        <p class="card-text">{{ $totalUnpublishedPosts }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ __('Total Number of Published Posts') }}</h5>
+                                        <p class="card-text">{{ $totalPublishedPosts }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
 </x-app-layout>
